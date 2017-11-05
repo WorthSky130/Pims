3081c6f1ec268ef21ebc1bf06cda8939db2888e9
“WorthSky130 <zhaoxuejiaows@sina.com>
2017-11-05 16:43:55 +0800

    dubbo 测试

---
+++ pims-manager-web/src/main/java/top/salutluoluo/controller/UserLoginController.java
@@ -0,0 +1,28 @@
+package top.salutluoluo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+/**
+ * 用户登录的controller
+ * @author lenovo
+ *
+ */
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import top.salutluoluo.pojo.TbUserLogin;
+import top.salutluoluo.service.UserLoginService;
+
+@Controller
+public class UserLoginController {
+    @Autowired
+    private UserLoginService userLoginService;
+	
+	@RequestMapping("/login/{userGlobalId}")
+	@ResponseBody
+	public TbUserLogin getUserLoginById(@PathVariable String userGlobalId){
+	TbUserLogin userLogin =  userLoginService.getUserLoginbyId(userGlobalId);
+	      return userLogin;
+	}
+}
